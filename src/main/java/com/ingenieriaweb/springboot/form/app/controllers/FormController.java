89f322b6b4c99506794ca07e0bd856fdb3e8727e
Richard Braul <richardzz@hotmail.com>
2022-06-28 01:42:27 -0500

    Atributo object de thymeleaf en form

--- src/main/java/com/ingenieriaweb/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/ingenieriaweb/springboot/form/app/controllers/FormController.java
@@ -64,19 +64,31 @@ public class FormController {
 //	}
 	
 //	Validacion del Fomulario usando anotaciones
+//	@PostMapping("/form")
+//	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+//		model.addAttribute("titulo","Resultado form");
+//		
+//		if(result.hasErrors()) {
+//			Map<String, String> errores = new HashMap<>();
+//			result.getFieldErrors().forEach(err -> 
+//			{
+//				errores.put(err.getField(),"El campo ".concat(err.getField().concat(" ").concat(err.getDefaultMessage())));
+//			});
+//			model.addAttribute("error",errores);
+//			return "form";
+//		}
+//		model.addAttribute("usuario",usuario);
+//		return "resultado";
+//	}
+	
 	@PostMapping("/form")
 	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 		model.addAttribute("titulo","Resultado form");
 		
-		if(result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err -> 
-			{
-				errores.put(err.getField(),"El campo ".concat(err.getField().concat(" ").concat(err.getDefaultMessage())));
-			});
-			model.addAttribute("error",errores);
+		if (result.hasErrors()) {
 			return "form";
 		}
+		
 		model.addAttribute("usuario",usuario);
 		return "resultado";
 	}
