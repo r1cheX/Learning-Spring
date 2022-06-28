959b46f2559abc3b4157c58d5d42691a269e67db
Richard Braul <richardzz@hotmail.com>
2022-06-28 00:03:06 -0500

    Proyecto para iniciar realizando formularios

---
+++ src/main/java/com/ingenieriaweb/springboot/form/app/EpSem04Application.java
@@ -0,0 +1,13 @@
+package com.ingenieriaweb.springboot.form.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpSem04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpSem04Application.class, args);
+	}
+
+}
