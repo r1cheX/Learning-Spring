8ef03fe228c426269698e006bbfb29e022e29189
Richard Braul <richardzz@hotmail.com>
2022-06-28 02:18:03 -0500

    Agregando validaciones

---
+++ src/test/java/com/ingenieriaweb/springboot/form/app/EpSem05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenieriaweb.springboot.form.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpSem05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
